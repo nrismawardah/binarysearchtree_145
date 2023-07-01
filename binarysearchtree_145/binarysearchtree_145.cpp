80625493ea51605cfb8752e15d965c2148556dd3
nrismawardah <nrismawardah@gmail.com>
2023-07-01 13:46:20 +0700

    Membuat metode search

--- binarysearchtree_145/binarysearchtree_145.cpp
+++ binarysearchtree_145/binarysearchtree_145.cpp
@@ -54,4 +54,20 @@ public:
 			parent->rightchild = newNode;				// Make the right child of the parent point to the new node
 		}
 	}
+
+
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		// This function searches the currentNode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
 };
