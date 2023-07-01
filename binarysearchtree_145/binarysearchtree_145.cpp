9af5b62f2b07af732f9c38d6ccd8db4cb3c46444
nrismawardah <nrismawardah@gmail.com>
2023-07-01 13:14:09 +0700

    Menetapkan nilai ke bidang data node baru

--- binarysearchtree_145/binarysearchtree_145.cpp
+++ binarysearchtree_145/binarysearchtree_145.cpp
@@ -31,5 +31,6 @@ public:
 	void insert(string element)		// Insert a node in a binary search tree
 	{
 		Node* newNode = new Node(element, NULL, NULL);	// Allocate memory for the new node
+		newNode->info = element;						// Assign value to the data field of the new node
 	}
 };
